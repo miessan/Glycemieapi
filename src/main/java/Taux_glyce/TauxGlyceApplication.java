283438b9a90c0134f93ceccd919f929dc4b3fd30
miessan <miessan02@gmail.com>
2023-08-04 20:32:20 +0000

    Mise a jours ajout des cors

--- src/main/java/Taux_glyce/TauxGlyceApplication.java
+++ src/main/java/Taux_glyce/TauxGlyceApplication.java
@@ -2,6 +2,9 @@ package Taux_glyce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TauxGlyceApplication {
@@ -9,5 +12,17 @@ public class TauxGlyceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TauxGlyceApplication.class, args);
 	}
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 
 }
